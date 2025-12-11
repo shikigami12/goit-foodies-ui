24d738b988e905449bb4e43fd3903e6516390f26
Dmytro Shirobokov <dnc.dmytro.shirobokov@gmail.com>
2025-12-11 14:16:29 +0200

    Refactor: remove unused global CSS and integrate TailwindCSS dependencies with sprite

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 export const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      {/* TODO: Add Hero, Categories, Recipes, Testimonials */}
+      <h1>Home Page 1312311312312</h1>
+        <button className="bg-red-600 text-red-800 px-4 py-2 rounded-lg">
+            Button
+        </button>
+
+        {/* TODO: Add Hero, Categories, Recipes, Testimonials */}
     </div>
   );
 };
